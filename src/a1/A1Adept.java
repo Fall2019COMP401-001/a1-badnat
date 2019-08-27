a3943b999f56970d8b8fb9cc5d0286faceb51f38
badnat <badnat@badnat-pc.wireless-1x.unc.edu>
2019-08-27 16:22:12 -0400

    hey kmp 3 <3

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,77 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int count = scan.nextInt();
 		
+		String[] items = new String[count];
+		double[] prices = new double[count];
+		
+		for(int i = 0; i < count; i++) {
+			items[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+		
+		int countP = scan.nextInt();
+		
+		String[][] names = new String[countP][2];
+		double[] totals = new double[countP];
+		
+		for(int i = 0; i < countP; i++) {
+			names[i][0] = scan.next();
+			names[i][1] = scan.next();
+			int countI = scan.nextInt();
+			
+			for(int j = 0; j < countI; j++) {
+				 int factor = scan.nextInt();
+				 totals[i] +=  factor * prices[findPrice(items, scan.next())];
+			}
+		}
+		
+		int big = findBiggest(totals);
+		int low = findLowest(totals);
+		
+		System.out.println("Biggest: " + names[big][0] + " " + names[big][1] + " (" + String.format("%.2f", totals[big]) + ")");
+		System.out.println("Smallest: " + names[low][0] + " " + names[low][1] + " (" + String.format("%.2f", totals[low]) + ")");
+		System.out.println("Average: " + String.format("%.2f", average(totals)));
 	}
+	
+	static int findPrice(String[] items, String item) {
+		
+		for(int i = 0; i < items.length; i++) {
+			if (items[i].equals(item)) {
+				return i;
+			}	
+		}
+		System.out.println("item not found");
+		return -1;
+	}
+	
+	static int findBiggest(double[] totals) {
+		int temp = 0;
+		for(int i = 0; i < totals.length; i++) {
+			if (totals[i] > totals[temp]) {
+				temp = i;
+			}
+		}
+		return temp;
+	}
+	
+	static int findLowest(double[] totals) {
+		int temp = 0;
+		for(int i = 0; i < totals.length; i++) {
+			if (totals[i] < totals[temp]) {
+				temp = i;
+			}
+		}
+		return temp;
+	}
+	
+	static double average(double[] totals) {
+		double temp = 0;
+		for(int i = 0; i < totals.length; i++) {
+			temp += totals[i];
+		}
+		return temp / totals.length;
+	}
+	
 }
